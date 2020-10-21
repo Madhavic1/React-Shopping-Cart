14291f5c08bcdc2f68b7aa334349f7e59b037111
Madhavi Mullapudi <madhavi.0108@gmail.com>
2020-10-20 23:12:13 -0700

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/"> React Shoppping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All Rights are reserved</footer>
+    </div>
+  );
+}
+
+export default App;
